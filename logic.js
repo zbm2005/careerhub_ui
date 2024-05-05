0272fbb3d9e84dc8328f2fe4cb07e498f46c0505
biswajit <malakar.bidut1975@gmail.com>
2024-05-05 19:19:12 +0530

    page1

---
+++ logic.js
@@ -0,0 +1,11 @@
+
+function upload() {
+	const fileUploadInput = document.querySelector("#myFile");
+	const file = fileUploadInput.files[0];
+  
+	// Upload the file to the server
+	// ...
+  
+	// Display a message to the user
+	alert("The file has been uploaded successfully!");
+  }
